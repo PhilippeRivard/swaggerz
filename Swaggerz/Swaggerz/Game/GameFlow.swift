6877521010c9eba9ae22d0634618af496d92d642
olieydt <StraightUpG@s-MacBook-Pro.local>
2015-12-21 13:53:57 -0500

    no message

---
+++ Swaggerz/Swaggerz/Game/GameFlow.swift
@@ -0,0 +1,14 @@
+//
+//  GameFlow.swift
+//  Swaggerz
+//
+//  Created by Oli Eydt on 2015-12-21.
+//  Copyright (c) 2015 Swaggerz Light. All rights reserved.
+//
+
+import UIKit
+
+class GameFlow: NSObject {
+   
+    init(
+}
---
+++ Swaggerz/Swaggerz/User/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  Swaggerz
+//
+//  Created by Oli Eydt on 2015-12-21.
+//  Copyright (c) 2015 Swaggerz Light. All rights reserved.
+//
+
+import UIKit
+
+public class User: NSObject {
+   //characteristics of user
+    private var userName: String
+    private var schoolName: String
+    private var uniYear: integer_t
+    
+    
+    init(userName: String, schoolName: String, var uniYear: integer_t){
+        self.userName=userName
+        self.schoolName=schoolName
+        self.uniYear=uniYear
+    }
+}
