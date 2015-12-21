fc355343823ee35b128cb6a4c70f6de58df9a9a3
olieydt <StraightUpG@s-MacBook-Pro.local>
2015-12-21 13:30:54 -0500

    Initial Xcode build

---
+++ Swaggerz/Swaggerz/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Swaggerz
+//
+//  Created by Oli Eydt on 2015-12-21.
+//  Copyright (c) 2015 Swaggerz Light. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swaggerz/Swaggerz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Swaggerz
+//
+//  Created by Oli Eydt on 2015-12-21.
+//  Copyright (c) 2015 Swaggerz Light. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
