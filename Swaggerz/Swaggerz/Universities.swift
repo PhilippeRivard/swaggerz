406871d90ddd497c49d8f09938ea08581d04df50
Philippe Rivard <philipperivard@Philippes-MacBook-Pro.local>
2016-01-11 14:54:53 -0500

    created universities class

---
+++ Swaggerz/Swaggerz/Universities.swift
@@ -0,0 +1,9 @@
+//
+//  Universities.swift
+//  Swaggerz
+//
+//  Created by Philippe Rivard on 1/11/16.
+//  Copyright © 2016 Swaggerz Light. All rights reserved.
+//
+
+import Foundation
---
+++ Swaggerz/Swaggerz/UniversityList.swift
@@ -0,0 +1,9 @@
+//
+//  UniversityList.swift
+//  Swaggerz
+//
+//  Created by Philippe Rivard on 1/11/16.
+//  Copyright © 2016 Swaggerz Light. All rights reserved.
+//
+
+import Foundation
